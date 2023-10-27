57e9a14cee689b8d800b7fcdfae6b2ba37b91cdf
SchuhBaum <SchuhBaum71@gmail.com>
2023-10-27 04:28:14 +0200

    v0.0.2

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,5 @@
 ## NoAutoRotationFix
-###### Version: 0.0.1
+###### Version: 0.0.2
 
 This is a mod for Elden Ring.
 
@@ -11,7 +11,7 @@ https://www.youtube.com/watch?v=0-MaByXTaxw
 
 ### Installation
 0. Download and install [Elden Mod Loader](https://www.nexusmods.com/eldenring/mods/117) and [Anti-Cheat Toggler](https://www.nexusmods.com/eldenring/mods/90/). Disable anti-cheat if not done already.
-1. Download the file `NoAutoRotationFix.zip` from [Releases](https://github.com/SchuhBaum/NoAutoRotationFix/releases/tag/v0.0.1).
+1. Download the file `NoAutoRotationFix.zip` from [Releases](https://github.com/SchuhBaum/NoAutoRotationFix/releases/tag/v0.0.2).
 2. Extract its content in the folder `[Steam]\SteamApps\common\ELDEN RING\Game\`.
 3. Start the game as normal. Make sure that the in-game option `Launch Setting` is set to `Play Offline`.  
 
@@ -25,5 +25,8 @@ If you have feedback, you can message me on Discord `@schuhbaum` or write an ema
 There are two licenses available - MIT and Unlicense. You can choose which one you want to use.
 
 ### Changelog
+v0.0.2:
+- The same bug happens when you lock-on. Fixed in this version.
+
 v0.0.1:
 - Initial release.
--- SourceCode/dllmain.cpp
+++ SourceCode/dllmain.cpp
@@ -8,7 +8,7 @@ using namespace ModUtils;
 using namespace std;
 
 const string author = "SchuhBaum";
-const string version = "0.0.1";
+const string version = "0.0.2";
 
 DWORD WINAPI MainThread(LPVOID lpParam) {
     Log("author " + author);
@@ -33,6 +33,9 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
     // modded:
     // C7 81 40010000 0000803F  --  mov [rcx+00000140],3F800000 where (float)1 = 0x3F800000
     // 90 90 90 90 90 90        --  6 times nop
+    //
+    // 0x3F800000 = (0)(0111111 1)(0000000 00000000 00000000) binary; as a float this gives
+    // 1 = (-1)^0 * 2^(01111111 - offset) * (1 + 0*2^-1 + 0*2^-2 + ...) where offset = 127 = 0111111
     vector<uint8_t> modded = { 0xC7, 0x81, 0x40, 0x01, 0x00, 0x00, 0x00, 0x00, 0x80, 0x3F, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
 
 	uintptr_t assembly_location = SigScan(vanilla);
@@ -41,11 +44,28 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
         CloseLog();
 	    return 0;
     }
-    
 	Replace(assembly_location, vanilla, modded);
-	CloseLog();
-	
+    
+    // when you lock-on then the same variable as above gets set to zero => replace
+    // with nops; otherwise you get the same brief auto rotation;
+    
+    // vanilla: 
+    // 74 0C                    --  je <current_address + 0C>
+    // 48 89 B9 40010000        --  mov [rcx+00000140],rdi              <-- replace
+    // E9 DD040000              --  jmp <current_address + 4DD> (I think)
+    vanilla = { 0x74, 0x0C, 0x48, 0x89, 0xB9, 0x40, 0x01, 0x00, 0x00, 0xE9, 0xDD, 0x04, 0x00, 0x00 };
+    modded =  { 0x74, 0x0C, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0xE9, 0xDD, 0x04, 0x00, 0x00 };
+    assembly_location = SigScan(vanilla);
+    
+    if (assembly_location == 0) {
+        Log("Assembly changes could not be applied.");
+        CloseLog();
+	    return 0;
+    }
+	Replace(assembly_location, vanilla, modded);
+    
     // are the return values used for anything?;
+	CloseLog();
     return 1;
 }
 
